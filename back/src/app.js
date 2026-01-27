7569505f7cf03f5f5029e067a3faeb50377750d3
harrycruz132 <harrycruz.1304@gmail.com>
2026-01-27 10:26:28 -0600

    supabaseClient added

--- back/src/app.js
+++ back/src/app.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const cors = require('cors');
 // Carga variables de entorno
 require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const { supabase } = require("./supabaseClient");
+
+
 
 const app = express();
 
 // --- MIDDLEWARES ---
 app.use(cors());
 app.use(express.json());
+
+
+
+app.get("/api/test-supabase", async (req, res) => {
+  const { data, error } = await supabase
+    .from("guests")
+    .select("*")
+    .limit(1);
+
+  if (error) return res.status(500).json({ error: error.message });
+  res.json({ ok: true, data });
+});
 
 // --- RUTA DE PRUEBA ---
 app.get('/status', (req, res) => {
